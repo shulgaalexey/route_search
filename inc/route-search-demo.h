d800e1b41432c912361a439b5fffabf76d56e18d
shulgaalexey <shulgaalexey@gmail.com>
2015-12-08 22:17:49 +0900

    Added sources

---
+++ inc/route-search-demo.h
@@ -0,0 +1,19 @@
+#ifndef __route-search-demo_H__
+#define __route-search-demo_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "route-search-demo"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.route-search-demo"
+#endif
+
+#endif /* __route-search-demo_H__ */
